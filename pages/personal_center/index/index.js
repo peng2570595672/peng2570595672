11811201980ff975ca0122e0d6e9b3b3e922987a
刘清杰 <1948346770@qq.com>
2019-12-19 11:32:23 +0800

    添加跳转

--- pages/personal_center/index/index.js
+++ pages/personal_center/index/index.js
@@ -2,8 +2,10 @@ const util = require('../../../utils/util.js');
 Page({
 	data: {
 	},
-	// 我的ETC
-	myEtc () {
-		util.go('/pages/personal_center/my_etc/my_etc');
+	// 跳转
+	go (e) {
+		let url = e.currentTarget.dataset['url'];
+		let type = e.currentTarget.dataset.type;
+		util.go(`/pages/personal_center/${url}/${url}`);
 	}
 });
