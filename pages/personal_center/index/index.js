e14645671ade5464a8dee4b8a35d2eab45adbb56
HUA2CE2 <490130691@qq.com>
2019-12-26 09:22:27 +0800

    1.个人中心首页优化

--- pages/personal_center/index/index.js
+++ pages/personal_center/index/index.js
@@ -1,11 +1,32 @@
 const util = require('../../../utils/util.js');
 Page({
 	data: {
+		showDetailWrapper: false,
+		showDetailMask: false,
 	},
 	// 跳转
 	go (e) {
 		let url = e.currentTarget.dataset['url'];
 		let type = e.currentTarget.dataset.type;
 		util.go(`/pages/personal_center/${url}/${url}`);
-	}
+	},
+	// 扫码
+	scan () {
+		// 只允许从相机扫码
+		wx.scanCode({
+			onlyFromCamera: true,
+			success: (res) => {
+			},
+			fail: (res) => {
+				util.showToastNoIcon('扫码失败');
+			}
+		});
+	},
+	// 显示详情
+	showDetail (e) {
+		this.setData({
+			showDetailWrapper: true,
+			showDetailMask: true
+		});
+	},
 });
