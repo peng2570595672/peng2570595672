75bfac4087565d8208144791fa76ea41cef878e9
cuimin <2396368525@qq.com>
2020-03-26 16:46:01 +0800

    城市服务

--- cities_services/pages/cities_services/cities_services.js
+++ cities_services/pages/cities_services/cities_services.js
@@ -1,8 +1,64 @@
+const util = require('../../../utils/util.js');
+
 const app = getApp();
 
 Page({
 	data:{
+		loginInfo: {},// 登录信息
 	},
 	onLoad(){
+		wx.removeStorageSync('information_validation');
+		this.login();
 	},
+	// 自动登录
+	login () {
+		util.showLoading();
+		// 调用微信接口获取code
+		wx.login({
+			success: (res) => {
+				util.getDataFromServer('consumer/member/common/applet/code', {
+					platformId: app.globalData.platformId, // 平台id
+					code: res.code // 从微信获取的code
+				}, () => {
+					util.hideLoading();
+					util.showToastNoIcon('登录失败！');
+				}, (res) => {
+					if (res.code === 0) {
+						util.hideLoading();
+						res.data['showMobilePhone'] = util.mobilePhoneReplace(res.data.mobilePhone);
+						this.setData({
+							loginInfo: res.data
+						});
+						// 已经绑定了手机号
+						if (res.data.needBindingPhone !== 1) {
+							app.globalData.userInfo = res.data;
+							app.globalData.openId = res.data.openId;
+							app.globalData.memberId = res.data.memberId;
+							app.globalData.mobilePhone = res.data.mobilePhone;
+						}
+					} else {
+						util.hideLoading();
+						util.showToastNoIcon(res.message);
+					}
+				});
+			},
+			fail: () => {
+				util.hideLoading();
+				util.showToastNoIcon('登录失败！');
+			}
+		});
+	},
+	go(e) {
+		if (!app.globalData.memberId || app.globalData.memberId.length === 0) {
+			wx.setStorageSync('login_info', JSON.stringify(this.data.loginInfo));
+			util.go('/pages/login/login/login');
+			return;
+		}
+
+		let url = e.currentTarget.dataset['url'];
+		if (url === 'pages/default/receiving_address/receiving_address') {
+			app.globalData.orderInfo.orderId = '';
+		}
+		util.go(`/${url}`);
+	}
 });
