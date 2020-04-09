569b09b9d04be3d59a4306010e4e517cbe1f3bdc
刘清杰 <1948346770@qq.com>
2020-04-09 10:50:03 +0800

    优化

--- pages/personal_center/owner_service/owner_service.js
+++ pages/personal_center/owner_service/owner_service.js
@@ -10,21 +10,21 @@ Page({
 				img: 'https://file.cyzl.com/g001/M05/E0/4A/oYYBAF36zsmAJT1KAAA1qnJOMf4027.svg',
 				title: '优惠洗车',
 				abbreviation: '洗车',
-				content: '享受会员超值洗车服务，一年最高可省192元；支持全国上万家洗车门店提供优惠洗车服务，适用于小轿车、SUV、商务车7座及其以下私家车车型。'
+				content: '享受ETC车主特惠洗车服务，一年最高可省192元；支持全国上万家洗车门店提供优惠洗车服务，适用于小轿车、SUV、商务车7座及其以下私家车车型。'
 			},
 			{
 				id: 1,
 				img: 'https://file.cyzl.com/g001/M05/E0/47/oYYBAF36znmAWHtwAAAVvzkvmGs826.svg',
 				title: '优惠代驾',
 				abbreviation: '代驾',
-				content: '根据不同城市适配滴滴代驾和E代驾，每使用并完成一笔代驾服务，即可获赠1张1-100元高速通行抵扣券；通行抵扣券可在微ETC公众号个人中心的服务卡券查看。'
+				content: '根据不同城市适配滴滴代驾和E代驾，每使用并完成一笔代驾服务，即可获赠1张1-100元高速通行抵扣券。'
 			},
 			{
 				id: 2,
 				img: 'https://file.cyzl.com/g001/M00/02/91/CgAAD1zmPLiAD785AAAN5PuoTHU995.svg',
 				title: '预约检车',
 				abbreviation: '检车',
-				content: 'ETC会员尊享年检不过全额退款特权，支持全国范围内合作的检测站；线上提前支付预约，线下出示核销二维码即可享受年检服务。'
+				content: 'ETC车主尊享年检不过全额退款特权，支持全国范围内合作的检测站；线上提前支付预约，线下出示核销二维码即可享受年检服务。'
 			},
 			{
 				id: 3,
