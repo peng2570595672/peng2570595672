b213cec140c1bc9d15e05fa59e9a339ed075a55f
刘清杰 <1948346770@qq.com>
2020-05-08 16:55:33 +0800

    8位车牌新增规则

--- pages/default/receiving_address/receiving_address.js
+++ pages/default/receiving_address/receiving_address.js
@@ -537,7 +537,7 @@ Page({
 			isOk = isOk && formData.currentCarNoColor === 1;
 			// 进行正则匹配
 			if (isOk) {
-				let xreg = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9A]{5}[DF]$)|([ADF][A-HJ-NP-Z0-9][0-9]{4}$))/;
+				let xreg = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF]$)|([ADF][A-HJ-NP-Z0-9][0-9]{4}$))/;
 				isOk = xreg.test(this.data.carNoStr);
 			}
 		} else {
