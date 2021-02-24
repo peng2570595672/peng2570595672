200136c74060bbb247b803c3d1ada35ba46eab60
liuqingjie <1301838446@qq.com>
2021-02-24 10:17:14 +0800

    驾乘险

--- pages/personal_center/basic_rights_and_interests/basic_rights_and_interests.js
+++ pages/personal_center/basic_rights_and_interests/basic_rights_and_interests.js
@@ -3,23 +3,21 @@ const app = getApp();
 Page({
 	data: {
 		interestsList: [
-			{img: 'icon_make_invoice', title: '开具通行费发票', url: 'make_invoice'},
-			{img: 'icon_query', title: '违章不限次数查询', url: 'violation_enquiry'},
-			{img: 'icon_customer_service', title: '在线客服', url: 'online_customer_service'},
-			{img: 'icon_make_invoice', title: '开具付费办理发票', url: 'collect_paid_up_interest'}
+			{img: 'icon_make_invoice', title: '开具付费办理发票', url: 'invoice_issued_list'},
+			{img: 'icon_query', title: '违章不限次数查询', appId: 'wx06a561655ab8f5b2', url: 'pages/base/redirect/index?routeKey=PC01_REDIRECT&autoRoute=CHECKILLEGAL&outsource=souyisou&wtagid=116.115.10'},
+			{img: 'icon_make_invoice', title: '开具通行费发票', appId: 'wx9040bb0d3f910004', url: 'pages/index/index'},
+			{img: 'driving_risk', title: '每月领取驾乘险', appId: 'wx06a561655ab8f5b2', url: 'pages/base/redirect/index?routeKey=WD_PAZZ&wtagid=116.115.27'},
+			{img: 'icon_customer_service', title: '在线客服', url: 'online_customer_service'}
 		]
 	},
-	onLoad () {
-	},
-	onShow () {
-	},
 	go (e) {
-		let url = e.currentTarget.dataset.url;
-		if (url === 'violation_enquiry') {
+		let index = e.currentTarget.dataset.index;
+		let item = this.data.interestsList[index];
+		if (item.appId) {
 			// 打开的小程序版本， develop（开发版），trial（体验版），release（正式版）
 			wx.navigateToMiniProgram({
-				appId: 'wx06a561655ab8f5b2',
-				path: 'pages/base/redirect/index?routeKey=PC01_REDIRECT&autoRoute=CHECKILLEGAL&outsource=souyisou&wtagid=116.115.10',
+				appId: item.appId,
+				path: item.url,
 				envVersion: 'release', // 正式版
 				fail () {
 					util.showToastNoIcon('调起小程序失败, 请重试！');
@@ -27,26 +25,12 @@ Page({
 			});
 			return;
 		}
-		if (url === 'online_customer_service') {
-			util.go(`/pages/web/web/web?type=${url}`);
+		if (item.url === 'online_customer_service') {
+			util.go(`/pages/web/web/web?type=${item.url}`);
 			return;
 		}
-		if (url === 'collect_paid_up_interest') {
-			util.go(`/pages/personal_center/invoice_issued_list/invoice_issued_list`);
-			return;
+		if (item.url === 'invoice_issued_list') {
+			util.go(`/pages/personal_center/${item.url}/${item.url}`);
 		}
-		if (url === 'collect_paid_up_interest') {
-			this.goMakeInvoice();
-		}
-	},
-	goMakeInvoice () {
-		wx.navigateToMiniProgram({
-			appId: 'wx9040bb0d3f910004',
-			path: 'pages/index/index',
-			envVersion: 'release', // 目前联调为体验版
-			fail () {
-				util.showToastNoIcon('调起票根小程序失败, 请重试！');
-			}
-		});
 	}
 });
