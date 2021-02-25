189c2fd300ef40d0b146bbeb69264d1602ae79f0
xiancai <xiancai@2dfire.com>
2021-02-25 16:25:23 +0800

    付费发票税号规则修改

--- pages/personal_center/basic_rights_and_interests/basic_rights_and_interests.js
+++ pages/personal_center/basic_rights_and_interests/basic_rights_and_interests.js
@@ -3,7 +3,7 @@ const app = getApp();
 Page({
 	data: {
 		interestsList: [
-			{img: 'icon_make_invoice', title: '开具付费办理发票', url: 'invoice_issued_list'},
+			{img: 'icon_make_invoice', title: '开具ETC办理发票', url: 'invoice_issued_list'},
 			{img: 'icon_query', title: '违章不限次数查询', appId: 'wx06a561655ab8f5b2', url: 'pages/base/redirect/index?routeKey=PC01_REDIRECT&autoRoute=CHECKILLEGAL&outsource=souyisou&wtagid=116.115.10'},
 			{img: 'icon_make_invoice', title: '开具通行费发票', appId: 'wx9040bb0d3f910004', url: 'pages/index/index'},
 			{img: 'driving_risk', title: '每月领取驾乘险', appId: 'wx06a561655ab8f5b2', url: 'pages/base/redirect/index?routeKey=WD_PAZZ&wtagid=116.115.27'},
--- pages/personal_center/invoice_issued_list/invoice_issued_list.js
+++ pages/personal_center/invoice_issued_list/invoice_issued_list.js
@@ -28,14 +28,16 @@ Page({
 	},
 	onClickHandle (e) {
 		let index = e.currentTarget.dataset.index;
-		let origin = 0; // 0 去开票， 1已开票
+		let origin = 1; // 0 去开票， 1已开票
 		let info = this.data.currentList[index];
-		let infoStr = JSON.stringify(info);
-		// 开票状态0未申请 1申请成功、-1申请失败、2开票成功、3开票失败，4开票成功签章失败
-		let invoiceStatus = info.invoiceStatus;
-		if (invoiceStatus === 1 || invoiceStatus === 2 || invoiceStatus === 4) {
-			origin = 1;
+		let orderId = info.orderId;
+		// 开票状态0-待申请 1申请成功、2-申请失败
+		if(info.invoiceStatus !== 1){
+			origin = 0;
+			info = {};
+			info.orderId = orderId;
 		}
+		let infoStr = JSON.stringify(info);
 		util.go(`/pages/personal_center/invoice_issued_detail/invoice_issued_detail?origin=${origin}&infoStr=${infoStr}`);
 	}
 });
