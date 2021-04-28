52731ae45b8e667be323e3b5c6d997ecd2e908e1
liuqingjie <1301838446@qq.com>
2021-04-28 13:45:50 +0800

    客车办理流程

--- pages/personal_center/coupon_details/coupon_details.js
+++ pages/personal_center/coupon_details/coupon_details.js
@@ -5,13 +5,7 @@ Page({
 		details: {}
 	},
 	onLoad () {
-		// serviceCardVoucherDetails
-		wx.setNavigationBarColor({
-			frontColor: '#000000',
-			backgroundColor: '#50DC92'
-		});
 		this.setData({
-			backgroundColor: '#50DC92',
 			details: app.globalData.serviceCardVoucherDetails
 		});
 	}
