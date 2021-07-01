3767117f872709b58822da598f9e68b185befc4e
liuqingjie <1301838446@qq.com>
2021-07-01 14:09:24 +0800

    修复bug

--- pages/personal_center/index/index.js
+++ pages/personal_center/index/index.js
@@ -60,11 +60,7 @@ Page({
 				}
 			});
 			if (JSON.stringify(app.globalData.myEtcList) !== '{}') {
-				let isActivation = app.globalData.myEtcList.filter(item => (item.obuStatus === 1 || item.obuStatus === 5) && item.obuCardType === 1); // 1 已激活  2 恢复订单  5 预激活
-				this.setData({
-					isShowNotice: !!app.globalData.myEtcList.length,
-					isActivation: !!isActivation.length
-				});
+				this.getIsShow();
 			}
 		} else {
 			// 公众号进入需要登录
@@ -179,16 +175,19 @@ Page({
 		const result = await util.getDataFromServersV2('consumer/order/my-etc-list', params);
 		if (result.code === 0) {
 			app.globalData.myEtcList = result.data;
-			let isActivation = result.data.filter(item => (item.obuStatus === 1 || item.obuStatus === 5) && (item.obuCardType === 1 || item.obuCardType === 21)); // 1 已激活  2 恢复订单  5 预激活
-			let isShowFeatureService = result.data.findIndex(item => item.isShowFeatureService === 1 && (item.obuStatus === 1 || item.obuStatus === 5)); // 是否有特色服务
-			this.setData({
-				isShowNotice: !!app.globalData.myEtcList.length,
-				isShowFeatureService: isShowFeatureService !== -1,
-				isActivation: !!isActivation.length
-			});
+			this.getIsShow();
 		} else {
 			util.showToastNoIcon(result.message);
 		}
+	},
+	getIsShow () {
+		let isActivation = app.globalData.myEtcList.filter(item => (item.obuStatus === 1 || item.obuStatus === 5) && (item.obuCardType === 1 || item.obuCardType === 21)); // 1 已激活  2 恢复订单  5 预激活
+		let isShowFeatureService = app.globalData.myEtcList.findIndex(item => item.isShowFeatureService === 1 && (item.obuStatus === 1 || item.obuStatus === 5)); // 是否有特色服务
+		this.setData({
+			isShowNotice: !!app.globalData.myEtcList.length,
+			isShowFeatureService: isShowFeatureService !== -1,
+			isActivation: !!isActivation.length
+		});
 	},
 	// 众包-获取用户推广码和订单红包数量
 	async getMemberCrowdSourcingAndOrder () {
