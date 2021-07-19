f9fba32348e19b84814aedd38183edee1c7c2a84
liuqingjie <1301838446@qq.com>
2021-07-19 14:31:47 +0800

    判断蓝牙是否已开启

--- utils/utils.js
+++ utils/utils.js
@@ -4,10 +4,26 @@ export function initProductName (orderInfo) {
 	if (orderInfo.productProcess === 1) return '微信支付'
 	return orderInfo.productName || '';
 }
+
+// 比较两个日期
 export function compareDate (date1, date2) {
 	date1 = date1.slice(0, 16).replace(new RegExp('-','g'), '/');
 	date2 = date2.slice(0, 16).replace(new RegExp('-','g'), '/');
 	const newDate1 = new Date(date1);
 	const newDate2 = new Date(date2);
 	return newDate1 < newDate2;
+}
+
+// 判断是否开启蓝牙
+export function isOpenBluetooth () {
+	return new Promise(function (resolve) {
+		wx.openBluetoothAdapter({
+			success: () => {
+				resolve(true);
+			},
+			fail: () => {
+				resolve(false);
+			}
+		});
+	})
 }
