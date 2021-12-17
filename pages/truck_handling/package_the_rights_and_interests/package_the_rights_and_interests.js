c8960517c8b1b97eb0cf62fd97d4074cc7e4c114
liuqingjie <1301838446@qq.com>
2021-12-17 10:00:40 +0800

    交行修复bug

--- pages/truck_handling/package_the_rights_and_interests/package_the_rights_and_interests.js
+++ pages/truck_handling/package_the_rights_and_interests/package_the_rights_and_interests.js
@@ -225,7 +225,7 @@ Page({
 	},
 	// 查看办理协议
 	onClickGoAgreementHandle () {
-		if (this.data.orderInfo?.base?.flowVersion === 7) {
+		if (this.data.listOfPackages[this.data.choiceIndex].flowVersion === 7) {
 			util.go('/pages/truck_handling/bocom_handle_protocol/bocom_handle_protocol');
 		} else {
 			util.go('/pages/truck_handling/agreement/agreement');
