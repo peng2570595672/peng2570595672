5b6f284f716c73058191932f37434d21f242bdc5
liuqingjie <1301838446@qq.com>
2022-04-14 18:33:26 +0800

    修改交行协议

--- pages/personal_center/user_agreement/user_agreement.js
+++ pages/personal_center/user_agreement/user_agreement.js
@@ -4,6 +4,7 @@ Page({
 	data: {
 		isPassengerCarActivation: false,
 		isTruckActivation: false,
+		isBcoTruckActivation: false,
 		isCharge: false,
 		isFree: false,
 		carAgreementList: [],
@@ -48,21 +49,38 @@ Page({
 			}
 		});
 		let isTruckActivation = app.globalData.myEtcList.findIndex(item => (item.obuStatus === 1 || item.obuStatus === 5) && item.isNewTrucks === 1); //  货车已激活
-		let isObuCardType = app.globalData.myEtcList.findIndex(item => (item.obuCardType === 1 || item.obuCardType === 21)); // 卡类型
-   console.log(isObuCardType,'==============================卡类型==========================================',isTruckActivation);
-	 if (isObuCardType) { // 其他卡
-	 let 	truckAgreementList = [
-		    {id: 0,name: '货车办理协议', update: 0},
+		let isBcoTruckActivation = app.globalData.myEtcList.findIndex(item => (item.obuStatus === 1 || item.obuStatus === 5) && item.isNewTrucks === 1 && item.flowVersion === 7); //  交行货车已激活
+		let isObuCardType = app.globalData.myEtcList.findIndex(item => (item.obuCardType === 1 || item.obuCardType === 21)); // 卡类型 (黔通 客车 & 易路通达货车)
+		console.log(isObuCardType,'==============================卡类型==========================================',isTruckActivation);
+		if (isObuCardType === -1) { // 其他卡
+			let truckAgreementList = [
+				{id: 0,name: '货车办理协议', update: 0},
 				{id: 2,name: '隐私协议', update: 0}
-	   	];
+			];
 			this.setData({
 				truckAgreementList: truckAgreementList
 			});
-  }
+		}
+		if (isBcoTruckActivation !== -1) {
+			// 交行二类户
+			const truckAgreementList = [
+				{id: 0,name: '货车办理协议', update: 0},
+				{id: 1,name: '黔通卡ETC用户协议', update: 0},
+				{id: 2,name: '隐私协议', update: 0},
+				{id: 101,name: '代收业务扣款授权书', update: 0},
+				{id: 102,name: '涉税声明', update: 0},
+				{id: 103,name: '买卖账户法律责任及惩戒措施相关条款', update: 0},
+				{id: 104,name: '个人人民币银行结算账户管理协议', update: 0}
+			];
+			this.setData({
+				truckAgreementList: truckAgreementList
+			});
+		}
 		this.setData({
 			isPassengerCarActivation,
 			isObuCardType: isObuCardType,
 			isTruckActivation: isTruckActivation !== -1,
+			isBcoTruckActivation: isBcoTruckActivation !== -1,
 			isQTAttribute,
 			isNotQTAttribute,
 			isQTNotAttribute,
@@ -79,11 +97,11 @@ Page({
 			];
      console.log(isObuCardType,'=============================');
 			if (isObuCardType) { // 其他卡不是黔通
-		      carAgreementList = [
-						{id: 0,name: '用户办理协议', update: 0, url: 'agreement/agreement', isShow: isNotQTNotAttribute || isNotQTAttribute},
-						{id: 1,name: '用户办理协议', update: 0, url: 'free_equipment_agreement/free_equipment_agreement', isShow: isQTNotAttribute},
-						{id: 5,name: '隐私协议', update: 0, url: 'privacy_agreement/privacy_agreement', isShow: true}
-					];
+				carAgreementList = [
+					{id: 0,name: '用户办理协议', update: 0, url: 'agreement/agreement', isShow: isNotQTNotAttribute || isNotQTAttribute},
+					{id: 1,name: '用户办理协议', update: 0, url: 'free_equipment_agreement/free_equipment_agreement', isShow: isQTNotAttribute},
+					{id: 5,name: '隐私协议', update: 0, url: 'privacy_agreement/privacy_agreement', isShow: true}
+				];
 			}
 			carAgreementList = carAgreementList.filter(item => item.isShow === true);
 
@@ -129,6 +147,9 @@ Page({
 		switch (index) {
 			case 0:
 				// 办理协议
+				if (this.data.isBcoTruckActivation) {
+					util.go('/pages/truck_handling/bocom_handle_protocol/bocom_handle_protocol');
+				}
 				util.go('/pages/truck_handling/agreement/agreement');
 				break;
 			case 1:
@@ -147,6 +168,22 @@ Page({
 			case 4:
 				// 征信授权书
 				util.go('/pages/truck_handling/truck_credit_investigation_authorization/truck_credit_investigation_authorization');
+				break;
+			case 101:
+				// 代收业务扣款授权书
+				util.go('/pages/truck_handling/bocom_deduction_collection_protocol/bocom_deduction_collection_protocol');
+				break;
+			case 102:
+				// 涉税声明
+				util.go('/pages/truck_handling/bocom_tallage_protocol/bocom_tallage_protocol');
+				break;
+			case 103:
+				// 买卖账户法律责任及惩戒措施相关条款
+				util.go('/pages/truck_handling/bocom_clause_protocol/bocom_clause_protocol');
+				break;
+			case 104:
+				// 个人人民币银行结算账户管理协议
+				util.go('/pages/truck_handling/bocom_management_protocol/bocom_management_protocol');
 				break;
 			default:
 				break;
