8290d46f67f059d534cc4a566e75265ab963c5fb
雨林 <519919597@qq.com>
2022-08-02 17:30:02 +0800

    移动积分兑换通行券1

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -109,7 +109,15 @@ Page({
 		isShowNotice: false, // 是否显示广告位
 		isActivityDate: false, // 是否活动期间
 		isActivityForBannerDate: false, // 是否是banner上线时间
-		dialogContent: {} // 弹窗内容
+		dialogContent: {}, // 弹窗内容
+		arr1: {
+			title: '您即将授权当前手机号使用中国移动账户登录上海分互链信息技术有限公司所有的“分互链积分平台”，如果您无法认同如下内容，请您点击“取消”并拒绝授权。如您点击“继续”：',
+			text1: '1、即视为您同意和授权中国移动向上海分互链信息技术有限公司提供账户数据接口以使上海分互链信息技术有限公司可以调用您在中国移动网站(www.10086.cn)的注册账户的登录信息，便于您直接使用您在中国移动网站的注册信息登录“分互链积分平台”。',
+			text2: '2、即视为您同意及授权（1）上海分互链信息技术有限公司通过数据接口实时读取您在中国移动的消费积分的剩余数量，以完成兑换畅由积分、集分宝或其他积分权益；（2）上海分互链信息技术有限公司将您在中国移动的消费积分的剩余数量，共享给小程序页面运营方世纪恒通科技股份限公司，用于为您在页面显示剩余积分数量。',
+			text3: '3、表明您已明确知晓上海分互链信息技术有限公司及“分互链积分平台”并非中国移动的关联公司或由中国移动运营，您使用“分互链积分平台”或上海分互链信息技术有限公司提供的其他服务的行为均与中国移动无关，您也不能就使用中国移动网站注册信息登录及使用“分互链积分平台”的后果要求中国移动承担任何责任。',
+			text4: '4、中国移动郑重提醒您保管好您在中国移动网站的注册登录信息，切勿向任何人透露您的账号、密码等相关信息。除非得到您的同意及授权，中国移动不会向任何第三方透露您的任何信息。',
+		},
+		movingIntegralControl: false
 	},
 	async onLoad() {
 		app.globalData.isTruckHandling = false;
@@ -314,19 +322,10 @@ Page({
 		}
 		// @cyl
 		if (item?.url === 'moving_integral') {
-			console.log(item?.url);
-			
-			// wx.showModal({
-			// 	title: '授权',
-			// 	content: '您即将授权当前手机号使用中国移动账户登录上海分互链信息技术有限公司所有的“分互链积分平台”，如果您无法认同如下内容，请您点击“取消”并拒绝授权。如您点击“继续”：1、即视为您同意和授权中国移动向上海分互链信息技术有限公司提供账户数据接口以使上海分互链信息技术有限公司可以调用您在中国移动网站(www.10086.cn)的注册账户的登录信息，便于您直接使用您在中国移动网站的注册信息登录“分互链积分平台”。2、即视为您同意及授权（1）上海分互链信息技术有限公司通过数据接口实时读取您在中国移动的消费积分的剩余数量，以完成兑换畅由积分、集分宝或其他积分权益；（2）上海分互链信息技术有限公司将您在中国移动的消费积分的剩余数量，共享给小程序页面运营方世纪恒',
-			// 	success: function(res) {
-			// 		if (res.confirm) {
-			// 			console.log('用户点击确定')
-			// 		} else if (res.cancel) {
-			// 			console.log('用户点击取消')
-			// 		}
-			// 	}
-			// })
+			this.setData({
+				movingIntegralControl: true
+			})
+			return
 		}
 	},
 	openXiaoEPinPin() {
@@ -1286,5 +1285,20 @@ Page({
 		wx.uma.trackEvent(orderInfo.isNewTrucks === 1 ? 'index_for_certificate_to_truck_package' :
 			'index_for_certificate_to_package');
 		util.go(`/pages/${path}/information_list/information_list`);
+	},
+	// 点击移动积分兑换ETC 高速通行券
+	btnMovingIntegral (e) {
+		this.setData({
+			movingIntegralControl: false
+		})
+		if (e.currentTarget.id === 'cancel') {
+			
+			console.log("点击取消");
+		} else{
+			wx.navigateTo({
+				url: "/pages/moving_integral/bound_changyou/bound_changyou"
+			})
+			console.log("点击确认");
+		}
 	}
 });
