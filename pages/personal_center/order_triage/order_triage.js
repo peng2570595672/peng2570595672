83bd7700adbde50df15413144de55aca2216ed41
liuqingjie <1301838446@qq.com>
2023-03-02 17:08:31 +0800

    去掉延时

--- pages/personal_center/order_triage/order_triage.js
+++ pages/personal_center/order_triage/order_triage.js
@@ -15,8 +15,6 @@ Page({
 		let that = this;
 		let url = e.currentTarget.dataset.url;
 		// 避免重复点击触发
-		util.fangDou(this,() => {
-			util.go(`/pages/personal_center/${url}/${url}?test=${that.data.test}`);
-		},1000);
+		util.go(`/pages/personal_center/${url}/${url}?test=${that.data.test}`);
 	}
 });
