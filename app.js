68288b95948d391b6c0f31b6c1f759b82c3bc948
liuqingjie <1301838446@qq.com>
2023-03-04 15:06:44 +0800

    修复bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 是否为测试 TODO
-export const IS_TEST = false; // false为正式接口地址，true为测试接口地址
+export const IS_TEST = true; // false为正式接口地址，true为测试接口地址
 const util = require('./utils/util.js');
 const uma = require('./utils/umtrack-wx.js');
 App({
