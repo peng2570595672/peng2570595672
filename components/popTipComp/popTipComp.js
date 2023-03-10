5ea129e4032c7591d6b8f031ec67cda61f03a1ab
雨林 <519919597@qq.com>
2023-03-10 22:39:02 +0800

    优化

--- components/popTipComp/popTipComp.js
+++ components/popTipComp/popTipComp.js
@@ -13,10 +13,10 @@ Component({
 		multipleSlots: true // 在组件定义时的选项中启用多slot支持
 	},
 	properties: {
-		tipObj: {
-			type: Object,
-			value: {}
-		}
+		// tipObj: {
+		// 	type: Object,
+		// 	value: {}
+		// }
 	},
 
 	data: {
