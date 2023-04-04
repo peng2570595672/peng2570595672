ad60164fae10a42dfc9fd97a13fb9339c11a8416
liuqingjie <1301838446@qq.com>
2023-04-04 09:42:53 +0800

    替换客服

--- pages/web/web/web.js
+++ pages/web/web/web.js
@@ -32,23 +32,7 @@ Page({
 			// url = `https://api.wesure.cn/app/carinsure-new/driving/home/index.html?companyId=SJHT&outerUserId=${app.globalData.memberId}&wtagid=104.210.3`;
 			// url = `https://static.wesure.cn/app2/h5-mobile/home?companyId=ETC&outerUserId=${app.globalData.memberId}&wtagid=${wtagid}`;
 		} else if (options.type === 'online_customer_service') {
-			let businessType = encodeURIComponent('ETC+在线客服');
-			let cars = wx.getStorageSync('cars');
-			let carStr = '';
-			if (cars) {
-				let reg = /([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9])([A-Z0-9]{2})([A-Z0-9]{2})/ig;
-				if (cars.indexOf('、') !== -1) {
-					cars = cars.split('、');
-					for (let car of cars) {
-						carStr += car.replace(reg, '$1**$3') + '_';
-					}
-					carStr = carStr.substring(0, carStr.length - 1);
-				} else {
-					carStr = cars.replace(reg, '$1**$3');
-				}
-				carStr = encodeURIComponent(carStr);
-			}
-			url = `https://jimi3-chat.jd.com/bot?venderId=1563354131534&BusinessType=${businessType}&RegStatus=1&UserId=${app.globalData.memberId}&PlateNo=${carStr}&userPin=${app.globalData.memberId}&UserTel=${app.globalData.mobilePhone}`;
+			url = `https://wpa1.qq.com/GhL5oRJ0?_type=wpa&qidian=true`;
 		} else if (options.type === 'heaiche') {
 			url = `https://dhwap.mlocso.com/haccarwash/html/carwash.html?app=07200053&p=cwash_game&mobile=${app.globalData.activityUrl}`;
 			this.setData({
