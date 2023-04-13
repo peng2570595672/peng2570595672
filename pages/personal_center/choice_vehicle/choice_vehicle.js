b83a4349e822305bdbb94154e31c1b5b865a62e4
liuqingjie <1301838446@qq.com>
2023-04-13 17:06:45 +0800

    权益商城自动登录

--- pages/personal_center/choice_vehicle/choice_vehicle.js
+++ pages/personal_center/choice_vehicle/choice_vehicle.js
@@ -12,7 +12,52 @@ Page({
 	async onLoad (options) {
 	},
 	async onShow () {
-		await this.getRightAccount();
+		if (app.globalData.userInfo.accessToken) {
+			await this.getRightAccount();
+		} else {
+			// 公众号进入需要登录
+			this.login();
+		}
+	},
+	// 自动登录
+	login () {
+		util.showLoading();
+		// 调用微信接口获取code
+		wx.login({
+			success: async (res) => {
+				const result = await util.getDataFromServersV2('consumer/member/common/applet/code', {
+					platformId: app.globalData.platformId, // 平台id
+					code: res.code // 从微信获取的code
+				});
+				if (result.code === 0) {
+					result.data['showMobilePhone'] = util.mobilePhoneReplace(result.data.mobilePhone);
+					this.setData({
+						loginInfo: result.data
+					});
+					// 已经绑定了手机号
+					if (result.data.needBindingPhone !== 1) {
+						app.globalData.userInfo = result.data;
+						app.globalData.openId = result.data.openId;
+						app.globalData.memberId = result.data.memberId;
+						app.globalData.mobilePhone = result.data.mobilePhone;
+						this.setData({
+							mobilePhone: result.data.mobilePhone
+						});
+						this.getRightAccount();
+					} else {
+						wx.setStorageSync('login_info', JSON.stringify(this.data.loginInfo));
+						util.go('/pages/login/login/login');
+						util.hideLoading();
+					}
+				} else {
+					util.showToastNoIcon(result.message);
+				}
+			},
+			fail: () => {
+				util.hideLoading();
+				util.showToastNoIcon('登录失败！');
+			}
+		});
 	},
 	async getRightAccount () {
 		const result = await util.getDataFromServersV2('/consumer/member/right/account', {
