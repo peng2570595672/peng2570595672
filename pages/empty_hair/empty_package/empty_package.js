b75ef2fbc0b20e3d53b7322111e1b76850a74992
雨林 <519919597@qq.com>
2023-04-25 23:06:17 +0800

    优化：空发套餐页

--- pages/empty_hair/empty_package/empty_package.js
+++ pages/empty_hair/empty_package/empty_package.js
@@ -39,15 +39,60 @@ Page({
 		contractStatus: undefined,
 		isLoaded: false, // 是否加载数据完成
 		getAgreement: false, // 是否接受协议
-		isPay: false // 已选择通通券套餐&无需支付||已经支付
+		isPay: false, // 已选择通通券套餐&无需支付||已经支付
+        isOnloadData: true
+    },
+    async onLoad (options) {
+        console.log(options);
+        if (app.globalData.userInfo.accessToken) {
+            // 根据套餐id查询套餐信息
+            await this.getProduct();
+        } else {
+            this.setData({
+                isOnloadData: false
+            });
+            this.login();
+        }
+    },
+    async onShow () {
+        if (!this.data.isOnloadData) {
+            // 根据套餐id查询套餐信息
+            await this.getProduct();
+        }
 	},
-	async onShow (res) {
-        console.log(res);
-        let result = wx.getLaunchOptionsSync();
-        console.log('参数：',result);
-        let extra = result.referrerInfo.extraData;
-        console.log('数据：',extra);
-        // await this.getProduct(extra.shopProductId);
+
+	// 自动登录
+	login () {
+		util.showLoading();
+		// 调用微信接口获取code
+		wx.login({
+			success: async (res) => {
+				const result = await util.getDataFromServersV2('consumer/member/common/applet/code', {
+					platformId: app.globalData.platformId, // 平台id
+					code: res.code // 从微信获取的code
+				});
+				if (!result) return;
+				if (result.code) {
+					util.showToastNoIcon(result.message);
+					return;
+				}
+				result.data['showMobilePhone'] = util.mobilePhoneReplace(result.data.mobilePhone);
+				// 已经绑定了手机号
+				if (result.data.needBindingPhone !== 1) {
+					app.globalData.userInfo = result.data;
+					app.globalData.openId = result.data.openId;
+					app.globalData.memberId = result.data.memberId;
+					app.globalData.mobilePhone = result.data.mobilePhone;
+				} else {
+					wx.setStorageSync('login_info', JSON.stringify(this.data.loginInfo));
+					// util.go('/pages/login/login/login');
+				}
+			},
+			fail: () => {
+				util.hideLoading();
+				util.showToastNoIcon('登录失败！');
+			}
+		});
 	},
 
 	// 查看办理协议
@@ -96,7 +141,7 @@ Page({
 			util.showToastNoIcon('请同意并勾选协议！');
 			return;
 		}
-		if (this.data.listOfPackages[this.data.choiceIndex].shopProductId === app.globalData.emptyPackageId) {
+		if (this.data.listOfPackages[this.data.choiceIndex].shopProductId === app.globalData.salesmanEmptyObj.shopProductId) {
 			this.emptySaveOrder();
         }
 	},
@@ -139,7 +184,7 @@ Page({
 				success: (res) => {
 					this.setData({isRequest: false});
 					if (res.errMsg === 'requestPayment:ok') {
-
+                        util.go(`/pages/empty_hair/empty_qrcode/empty_qrcode`);
 					} else {
 						util.showToastNoIcon('支付失败！');
 					}
@@ -277,10 +322,9 @@ Page({
 		});
 	},
 	// 根据套餐id获取套餐信息
-	async getProduct (obj) {
-		console.log(obj);
+	async getProduct () {
 		const result = await util.getDataFromServersV2('consumer/system/get-product-by-id', {
-			shopProductId: obj
+			shopProductId: app.globalData.salesmanEmptyObj.shopProductId
 		});
 		if (!result) return;
 		if (result.code === 0) {
@@ -290,10 +334,10 @@ Page({
 			});
 			this.getNodeHeight(this.data.listOfPackages.length);
 			// 未登录
-		if (!app.globalData.userInfo?.accessToken) {
-			wx.setStorageSync('login_info', JSON.stringify(this.data.loginInfo));
-			util.go('/pages/login/login/login');
-		}
+            // if (!app.globalData.userInfo?.accessToken) {
+            // 	wx.setStorageSync('login_info', JSON.stringify(this.data.loginInfo));
+            // 	util.go('/pages/login/login/login');
+            // }
 		} else {
 			util.showToastNoIcon(result.message);
 		}
