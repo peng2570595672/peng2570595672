a937ba0a6d49a1308bda5d4b54658ad58909f1f8
雨林 <519919597@qq.com>
2023-09-01 09:39:40 +0800

    优化

--- pages/road_rescue_orders/road_rescue_schedule/road_rescue_schedule.js
+++ pages/road_rescue_orders/road_rescue_schedule/road_rescue_schedule.js
@@ -14,26 +14,26 @@ Page({
     },
 
     onShow () {
-        this.getApplyInfo();
+        if (this.data.applyId) {
+            this.getApplyInfo(this.data.applyId);
+        } else {
+            this.getOrderInfo();
+        }
+    },
+    // 获取订单信息
+    async getOrderInfo () {
+        const result = await util.getDataFromServersV2('consumer/order/single-road-rescue', {orderId: this.data.orderId},'POST',true);
+		if (!result) return;
+		if (result.code === 0) {
+            this.getApplyInfo(result.data.applyId);
+		} else {
+			util.showToastNoIcon(result.message);
+		}
     },
 
     // 获取申请进度以及申请信息
-    async getApplyInfo () {
-        // let applyInfo = {
-        //     applyNo: '432432432432',// 申请补贴流水号
-        //     roadRescueNo: '657657657575',// 领取流水号
-        //     applyStatus: 3,// 申请补贴状态(0:待申请;1:审核中;2:已通过;3:未通过;)
-        //     applyUpdatTime: '2023-01-24 12:23:22',// 补贴状态更新时间
-        //     remark: '信息有误',// 未通过原因
-        //     receiveTime: '',// 补贴领取时间
-        //     applyInfoObj: {
-        //         rescueTime: '2023-01-23 16:23:22',// 呼叫救援时间
-        //         receiveName: '雨林',// 收款人姓名
-        //         bankNo: '3243243432432432',// 卡号
-        //         rescueMoney: 500 // 金额(分)
-        //     }
-        // };
-        const result = await util.getDataFromServersV2('consumer/order/road-rescue/schedule', {id: this.data.applyId},'POST',true);
+    async getApplyInfo (obj) {
+        const result = await util.getDataFromServersV2('consumer/order/road-rescue/schedule', {id: obj},'POST',true);
 		if (!result) return;
 		if (result.code === 0) {
             this.setData({applyInfo: result.data});
