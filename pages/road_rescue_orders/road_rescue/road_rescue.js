d4cb9df3d9945485ef6d95fba134ff852b9c63a4
雨林 <519919597@qq.com>
2024-01-25 17:21:56 +0800

    注释掉道路救援的测试用例

--- pages/road_rescue_orders/road_rescue/road_rescue.js
+++ pages/road_rescue_orders/road_rescue/road_rescue.js
@@ -68,79 +68,79 @@ Page({
         if (!result) return;
         if (result.code === 0) {
             let roadRescueList = [
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: null,
-                    applicationStatus: null,
-                    vehPlates: '贵ZQ0101',
-                    roadRescueStatus: 0,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 0,
-                    applicationStatus: 0,
-                    vehPlates: '贵ZQ0102',
-                    roadRescueStatus: 1,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 1,
-                    applicationStatus: 0,
-                    vehPlates: '贵ZQ0103',
-                    roadRescueStatus: 2,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 1,
-                    applicationStatus: 0,
-                    vehPlates: '贵ZQ0104',
-                    roadRescueStatus: 3,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 1,
-                    applicationStatus: 4,
-                    vehPlates: '贵ZQ0105',
-                    roadRescueStatus: 4,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 1,
-                    applicationStatus: 5,
-                    vehPlates: '贵ZQ0106',
-                    roadRescueStatus: 5,
-                    rescueMoney: 50000,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: 1,
-                    applicationStatus: 6,
-                    vehPlates: '贵ZQ0107',
-                    roadRescueStatus: 6,
-                    orderId: '3213231321313'
-                },
-                {
-                    receiveTime: '2023.07.03 12:12:12',// 领取时间
-                    expirationTime: '2024.07.03 12:12:12',// 到期时间
-                    isReceive: null,
-                    applicationStatus: null,
-                    vehPlates: '贵ZQ0108',
-                    roadRescueStatus: 7,
-                    orderId: '3213231321313'
-                }
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: null,
+                //     applicationStatus: null,
+                //     vehPlates: '贵ZQ0101',
+                //     roadRescueStatus: 0,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 0,
+                //     applicationStatus: 0,
+                //     vehPlates: '贵ZQ0102',
+                //     roadRescueStatus: 1,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 1,
+                //     applicationStatus: 0,
+                //     vehPlates: '贵ZQ0103',
+                //     roadRescueStatus: 2,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 1,
+                //     applicationStatus: 0,
+                //     vehPlates: '贵ZQ0104',
+                //     roadRescueStatus: 3,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 1,
+                //     applicationStatus: 4,
+                //     vehPlates: '贵ZQ0105',
+                //     roadRescueStatus: 4,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 1,
+                //     applicationStatus: 5,
+                //     vehPlates: '贵ZQ0106',
+                //     roadRescueStatus: 5,
+                //     rescueMoney: 50000,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: 1,
+                //     applicationStatus: 6,
+                //     vehPlates: '贵ZQ0107',
+                //     roadRescueStatus: 6,
+                //     orderId: '3213231321313'
+                // },
+                // {
+                //     receiveTime: '2023.07.03 12:12:12',// 领取时间
+                //     expirationTime: '2024.07.03 12:12:12',// 到期时间
+                //     isReceive: null,
+                //     applicationStatus: null,
+                //     vehPlates: '贵ZQ0108',
+                //     roadRescueStatus: 7,
+                //     orderId: '3213231321313'
+                // }
             ];
             roadRescueList = roadRescueList.concat(result.data);
             let isNoData = !result.data || result.data.length === 0;
