e4e42f492a9fe7c67cc30ef7a1ec50636a61cbe9
雨林 <519919597@qq.com>
2024-03-25 14:15:35 +0800

    修复轮播图图片展示出现上下拉伸bug

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -417,11 +417,11 @@ Page({
         const winWid = wx.getSystemInfoSync().windowWidth; // 获取当前屏幕的宽度
         const imgh = e.detail.height; // 图片高度
         const imgw = e.detail.width;
-        let swiperH = (winWid * imgh / imgw) * 2; // 等比设置swiper的高度。  即 屏幕宽度 / swiper高度 = 图片宽度 / 图片高度    ==》swiper高度 = 屏幕宽度 * 图片高度 / 图片宽度
+        let swiperH = (winWid * imgh / imgw); // 等比设置swiper的高度。  即 屏幕宽度 / swiper高度 = 图片宽度 / 图片高度    ==》swiper高度 = 屏幕宽度 * 图片高度 / 图片宽度
         this.data.HeiList.push(swiperH);
         swiperH = Math.max(...this.data.HeiList); // 获取最大的值
         this.setData({
-            Hei: swiperH + 'rpx' // 设置高度
+            Hei: swiperH + 'px' // 设置高度
         });
     },
     onHandleNotice (info) {
