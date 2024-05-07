534b765dba3cbf929b1b23e0eaba768b95d1c6ea
liuqingjie <1301838446@qq.com>
2024-05-07 14:29:15 +0800

    货车流程优化-去掉身份证校检

--- pages/truck_handling/upload_id_card/upload_id_card.js
+++ pages/truck_handling/upload_id_card/upload_id_card.js
@@ -171,10 +171,9 @@ Page({
 		if (this.data.isRequest) {
 			return;
 		}
-		if (app.globalData.test) {
-			this.next();
-			return;
-		}
+		this.next();
+	},
+	async checkIdCardAndVehPlate () {
 		// 校验货车身份证信息是否一致
 		let data = {
 			vehPlate: this.data.options.vehPlates,
