0bdafe69e516f4e5c4ded8a014d0a0208279c5ff
pengyi-cheng <2570595672@qq.com>
2024-05-10 17:28:42 +0800

    删除多余代码

--- components/viewIsTruck/viewIsTruck.js
+++ components/viewIsTruck/viewIsTruck.js
@@ -16,38 +16,10 @@ Component({
 		isShow: false
 	},
 	methods: {
-		generateValidLuhnChecksum (length) {
-			let sum = 0;
-			let double = false;
-			let number = '';
-
-			for (let i = 1; i <= length - 2; i++) {
-				number += Math.floor(Math.random() * 10).toString();
-
-				if (double) {
-					let digit = parseInt(number.charAt(i - 1));
-					digit *= 2;
-					if (digit > 9) {
-						digit -= 9;
-					}
-					sum += digit;
-				} else {
-					sum += parseInt(number.charAt(i - 1));
-				}
-
-				double = !double;
-			}
-
-			const checkDigit = 10 - (sum % 10);
-			return `${number}${checkDigit}`;
-		},
 		ok (e) {
 			this.hide(e, true);
 		},
 		show () {
-			// 调用你的luhmCheck函数
-			console.log('19位银行卡号校验结果',util.luhmCheck('6228481198890115877'));
-			// console.log(`19位银行卡号校验结果: ${util.luhmCheck('6228481198890115877')}`);
 			let height = wx.getSystemInfoSync().screenHeight / wx.getSystemInfoSync().screenWidth;
 			console.log();
 			this.setData({
