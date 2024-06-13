3dad1ecb6fe9a7c2262fcdc0253690085b999fc4
pengyi-cheng <2570595672@qq.com>
2024-06-13 15:04:50 +0800

    优化道路救援部分 账单优化 还原换卡不发部分

--- pages/personal_center/roadRelief/roadRelief.js
+++ pages/personal_center/roadRelief/roadRelief.js
@@ -5,64 +5,6 @@ Page({
      * 页面的初始数据
      */
     data: {
-        sourceOfServiceProvider: '1', // 服务商来源，举值:星星朗、天安保险，
-        roadRescueList: [
-            {
-                roadRescueStatus: '1',
-                details: [
-                    {
-                        label: '驾乘意外身故',
-                        time: '2024-05-20 14:10:32',
-                        highway: '2024-05-20 14:10:32'
-                    },
-                    {
-                        label: '驾乘意外医疗',
-                        time: '广深高速',
-                        highway: '贵州高速'
-                    },
-                    {
-                        label: '权益服务费',
-                        time: '',
-                        highway: '580元'
-                    },
-                    {
-                        label: '计算方式',
-                        time: '',
-                        highway: '通行费*6%'
-                    }
-                ]
-            }, {
-                roadRescueStatus: '1',
-                details: [
-                    {
-                        label: '驾乘意外身故',
-                        time: '2024-05-20 14:10:32',
-                        highway: '2024-05-20 14:10:32'
-                    },
-                    {
-                        label: '驾乘意外医疗',
-                        time: '广深高速',
-                        highway: '贵州高速'
-                    },
-                    {
-                        label: '住院津贴',
-                        time: '1000元/天',
-                        highway: ''
-                    },
-                    {
-                        label: '免赔300元后',
-                        time: '',
-                        highway: '90%赔付'
-                    },
-                    {
-                        label: '增值服务',
-                        time: '',
-                        highway: '免费拖车100公里'
-                    }
-                ]
-            }
-            // 更多道路救援项目...
-        ]
     },
 
     /**
@@ -89,7 +31,7 @@ Page({
     },
     validateCar () {
         wx.makePhoneCall({
-            phoneNumber: '1234567890', // 需要拨打的电话号码
+            phoneNumber: '12122', // 需要拨打的电话号码
             success () {
                 // 拉起拨打成功后的回调
             },
@@ -103,41 +45,6 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage () {
 
     }
 });
