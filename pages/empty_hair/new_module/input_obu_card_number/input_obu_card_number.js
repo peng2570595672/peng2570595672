b1037ae65328c280f4fed0481e87fb5cd06a17c5
雨林 <519919597@qq.com>
2024-06-14 11:01:31 +0800

    修改“录入OBU和卡签号”的样式与逻辑

--- pages/empty_hair/new_module/input_obu_card_number/input_obu_card_number.js
+++ pages/empty_hair/new_module/input_obu_card_number/input_obu_card_number.js
@@ -1,66 +1,99 @@
-// pages/empty_hair/new_module/input_obu_card_number/input_obu_card_number.js
+const app = getApp();
+const util = require('../../../../utils/util.js');
+const compressPicturesUtils = require('../../../../utils/compress_pictures_utils.js');
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
+        available: false,
+        obuNumber: '', // obu号码 sn
+        kaNumber: '' // 卡签号码
+    },
+
+    onLoad (options) {
 
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
+    onShow () {
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
+    inputCode (e) {
+        console.log(e);
+        this.setData({obuNumber: e.detail.value.trim()});
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
+    // 拍照或选择图片
+    chooseImage (e) {
+        let that = this;
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original', 'compressed'],
+            sourceType: ['album', 'camera'],
+            success: (res) => {
+                that.uploadCardOrcFile(res.tempFilePaths[0]);
+            },
+            fail: (res) => {
+                if (res.errMsg !== 'chooseImage:fail cancel') {
+                    util.showToastNoIcon('选择图片失败！');
+                }
+            }
+        });
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
+    // 裁剪压缩图片 并缓存
+    uploadCardOrcFile (path) {
+        let that = this;
+        let picPath = '';
+        // 裁剪压缩图片
+        compressPicturesUtils.processingPictures(that, path, 'pressCanvas', 640, (res) => {
+            picPath = res ? res : path;
+        });
+        that.uploadOcrFile(picPath || path,14);
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
+    // 识别图片
+    uploadOcrFile (path, type, selfType) {
+        util.showLoading();
+        let that = this;
+        util.uploadOcrFile(path, type, () => {
+           util.showToastNoIcon('上传失败');
+        }, (res) => {
+            if (!res) {
+                wx.hideLoading();
+                util.showToastNoIcon('文件服务器异常');
+                return;
+            }
+            res = JSON.parse(res);
+            if (res.code === 0) {
+                let ocrObject = res.data[0].ocrObject;
+                console.log('数据：',Object.values(ocrObject));
+                Object.values(ocrObject).map(item => {
+                    if (item.includes('SN号')) {
+                        that.setData({
+                            obuNumber: item.split(':')[1],
+                            available: true
+                        });
+                    }
+                    if (item.includes('卡号')) {
+                        that.setData({
+                            kaNumber: item.split(':')[1]
+                        });
+                    }
+                    wx.hideLoading();
+                });
+            } else {
+                wx.hideLoading();
+                util.showToastNoIcon(res.message);
+            }
+        }, () => {}, (res) => {});
     },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
+    next () {
+        if (!this.data.available) return;
+        console.log();
     },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
+    onUnload () {
 
     }
-})
+});
